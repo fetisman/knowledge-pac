14b2433659d1586c248a40582ae98f361462bda4
fetisman <fetismanjava@gmail.com>
2025-03-31 15:59:32 +0300

    ability to add and validate pac / deploy script

--- src/main/java/com/fetisman/model/Kpac.java
+++ src/main/java/com/fetisman/model/Kpac.java
@@ -1,15 +1,21 @@
 package com.fetisman.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import java.time.LocalDate;
 
 @Data
 public class Kpac {
     private int id;
-    private String title;
-    private String description;
+    @NotBlank private String title;
+    @NotBlank private String description;
+    @NotNull
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate creationDate;
-    private boolean last;
 }
 
